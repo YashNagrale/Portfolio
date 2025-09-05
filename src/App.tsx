004238e1f57934679c6cc117cf63f65a7ae2a50b
YashNagrale <ynagrale668@gmail.com>
2025-09-05 21:04:13 +0530

    feat: enhance styling and typography; update index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Yash Nagrale</h1>
+
+      <p className="p-2 border-2 border-white font-sans">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint optio
+        beatae aperiam doloribus eos facilis saepe, corporis, excepturi non
+        ipsum dolorum labore illo temporibus rem iusto reprehenderit? Amet,
+        alias vel id voluptates praesentium quo similique impedit voluptatum
+        nulla. Reprehenderit repudiandae, mollitia veniam nam a quas temporibus
+        voluptatibus fugiat aut. Enim voluptas aspernatur ex in, incidunt
+        molestiae eos corporis sit rerum doloribus assumenda aut saepe sunt
+        inventore, quasi quas modi dolorem vel architecto sint voluptatibus
+        sequi accusamus. Unde, incidunt ex quos ratione amet molestias nesciunt
+        eius totam nemo pariatur animi dicta perferendis! Rem aspernatur
+        quibusdam aliquid beatae repellat dolorum harum. Obcaecati.
+      </p>
+    </div>
+  );
 }
 
 export default App;
