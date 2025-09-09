4bb127e880ba0e0969784f14c4ca5e754b1c5c25
YashNagrale <ynagrale668@gmail.com>
2025-09-09 21:02:47 +0530

    refactor: update Header component layout and styling; adjust border and heading size

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Nav, { Header } from "./components";
 
 function App() {
   return (
-    <div id="container" className="md:max-w-3xl border-2 border-t-0 mx-auto">
+    <div id="container" className="md:max-w-3xl border-1 border-t-0 mx-auto">
       <Nav />
       <Header />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,23 @@ import { Verified } from "lucide-react";
 
 function Header() {
   return (
-    <header className="flex items-center gap-0.5">
-      <h1
-        className="text-4xl font-['Bungee'] p-5 border-r
-            diagonal-lines"
-      >
-        YN
-      </h1>
+    <header>
+      <div className="flex">
+        <h1 className="text-6xl font-['Bungee'] p-5 border-r diagonal-lines">
+          YN
+        </h1>
 
-      <p className="text-3xl font-bold pl-2">Yash Nagrale</p>
-      <Verified className="w-6 h-6 text-background fill-blue-500 relative top-[2px]" />
+        <div className="content-end w-full">
+          <div className="flex items-center gap-0.5 py-2">
+            <p className="text-3xl font-bold pl-3">Yash Nagrale</p>
+            <Verified className="w-6 h-6 text-background fill-blue-500 relative top-[2px]" />
+          </div>
+
+          <p className="text-muted-foreground font-mono py-1 text-sm pl-3 border-t w-full">
+            Full stack web developer.
+          </p>
+        </div>
+      </div>
     </header>
   );
 }
